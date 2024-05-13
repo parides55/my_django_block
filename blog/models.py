3c8a985aea999ee194526ce7b36ed59dc86fc24a
parides55 <andreasparides5@gmail.com>
2024-05-13 07:17:04 +0300

    return a string from the comment model

--- blog/models.py
+++ blog/models.py
@@ -14,6 +14,12 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f"The title of this post is {self.title} | written by {self.author}"
+
+    class Meta:
+        ordering = ['-created_on', 'author']
+
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
@@ -21,3 +27,9 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
+
+    class Meta:
+        ordering = ['-created_on']
