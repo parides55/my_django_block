639bdae1348d65d67e6c2c17dc1811d9dd31756f
parides55 <andreasparides5@gmail.com>
2024-05-15 17:50:08 +0100

    display the excerpt in the index.html

--- blog/models.py
+++ blog/models.py
@@ -27,7 +27,6 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
-    
 
     def __str__(self):
         return f"Comment {self.body} by {self.author}"
