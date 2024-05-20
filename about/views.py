5fb4a4488a9d72900187a06c5e97e716f7647be0
parides55 <andreasparides5@gmail.com>
2024-05-20 12:00:09 +0100

    create a collaborator form and model

--- about/views.py
+++ about/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import About
+from .forms import CollaborateForm
 
 # Create your views here.
 def about_me(request):
@@ -8,9 +9,11 @@ def about_me(request):
     """
 
     about = About.objects.all().order_by('-updated_on').first()
+    collaborate_form = CollaborateForm()
 
     return render(
         request,
         "about/about.html",
-        {"about": about},
+        {"about": about,
+         "collaborate_form": collaborate_form},
     )
